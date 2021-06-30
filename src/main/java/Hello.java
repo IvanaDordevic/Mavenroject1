0e4d99ae98bfddaa8a560721086cda63ab69901c
Ivana Dordevic <ivanadordevic.usa@gmail.com>
2021-06-30 18:47:42 -0400

    Additions for Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,6 @@ public class Hello {
         System.out.println("Hello");
         System.out.println("Developing Hello");
         System.out.println("Local repo is already published");
-        System.out.println("doing more work");
+
     }
 }
